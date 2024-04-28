315c399301c03db4cce9808f0a9e2ffe498546e8
ocatviope <octav.petrea@gmail.com>
2024-04-28 20:35:50 +0200

    LDEI funcion verificar

--- LDEI.py
+++ LDEI.py
@@ -42,7 +42,40 @@ class LDEI:
             
             return self._z
            
-    
+    def verificar(self, q: int, p: int, g: list[int], alpha: list[int], k: int, x: list[int]):
+        
+        m = len(self._a) 
+
+        if(len(alpha) != m or len(x) != m or len(g) != m): 
+            print("Verificacion fallida longitud incorrecta.")
+            return False
+        
+        if(gf_degree(self._z) > k):
+            print("Verificacion fallida grado de z incorrecto.")
+            return False
+        
+         # Operaciones mod q
+        hash = Hash().hash_ZZp(q, x, self._a)
+        if (self._e != hash):
+            print("Verificacion fallida digest incorrecto.")
+            return False
+        
+        zi = gf_multi_eval(self._z, alpha, q, ZZ)
+
+        # Operaciones mod p
+        tmp1, tmp2, tmp3 = 0, 0, 0
+        for i in range(m):
+            tmp2 = (pow(g[i], int(zi[i]), p))
+            tmp3 = (pow(x[i], int(self._e), p))
+            tmp1 = gf_mul([tmp3], [self._a[i]], p, ZZ)[0]
+            print("tmp1: ", tmp1)
+            print("tmp2: ", tmp2)
+            print("tmp3: ", tmp3)
+            print("self._a: ", self._a)
+            if (tmp2 != tmp1):
+                print("Verificacion fallida a_i incorrecto.")
+                return False
+        return True   
         
 
            
