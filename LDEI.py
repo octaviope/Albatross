e5dd60e6bd9aa52f5bc173e3fe1f8c1f83d1b9ab
ocatviope <octav.petrea@gmail.com>
2024-05-06 20:32:34 +0200

    localldei

--- LDEI.py
+++ LDEI.py
@@ -40,7 +40,7 @@ class LDEI:
             tmp = gf_mul([self._e], P, q, ZZ)
             self._z = gf_add(tmp, R, q, ZZ)
             
-            return self._z
+            
            
     def verificar(self, q: int, p: int, g: list[int], alpha: list[int], k: int, x: list[int]):
         
@@ -68,15 +68,42 @@ class LDEI:
             tmp2 = (pow(g[i], int(zi[i]), p))
             tmp3 = (pow(x[i], int(self._e), p))
             tmp1 = gf_mul([tmp3], [self._a[i]], p, ZZ)[0]
-            print("tmp1: ", tmp1)
+            print("tmp1: ", tmp1)###########################################
             print("tmp2: ", tmp2)
             print("tmp3: ", tmp3)
             print("self._a: ", self._a)
             if (tmp2 != tmp1):
                 print("Verificacion fallida a_i incorrecto.")
                 return False
-        return True   
-        
+        return True
 
-           
-            
+    def localldei(q: int, p: int, alpha: list, k: int, x: list ,m: int): #Creo que es un método estático.
+        # Operaciones mod q
+        u = [0] * m
+        for i in range(m):
+            prod = 1
+            for l in range(m):
+                if(l != i):
+                    tmp = (alpha[i]-alpha[l]) % q
+                    prod = prod * tmp
+            u[i] = 1 / prod 
+
+        # Polinomio aleatorio
+        P  = [ random.randint(0, q) for i in range(0, m - k - 1) ]
+        
+        # Calculo de v
+        v = [0] * m  
+        for i in range(m):
+            tmp = gf_multi_eval(P, alpha, q, ZZ) 
+            v[i] = (u[i] * tmp) % q
+
+        # Operaciones mod p
+        # Verificación
+        prod = 1
+        for i in range(m):
+            tmp = pow(x[i], v[i], p)
+            prod = (prod * tmp) % P
+
+        return
+        
+        
